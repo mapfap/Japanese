0a1a5b3b6f6da1d51f7638acff5c5ecf7093c856
Sarun Wongtanakarn <2062638+mapfap@users.noreply.github.com>
2019-10-05 14:07:42 +0700

    Improve performance

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import nagisa
 import romkan
 
 jmd = Jamdict()
+lookup_cache = {}
 
 lines = []
 for line in open('l1.txt', 'r').readlines():
@@ -18,12 +19,15 @@ print('***********************************************')
 
 for line in lines:
   for word in line:
-    entries = jmd.lookup(word).entries
+    entries = None
+    if word not in lookup_cache:
+      lookup_result = jmd.lookup(word)
+      entries = lookup_result.entries
     if entries:
-      entry = jmd.lookup(word).entries[0] 
+      entry = lookup_result.entries[0] 
       kana = entry.kana_forms[0].text if entry.kana_forms else ''
       romaji = romkan.to_roma(kana)
-      sense = entry.senses[0].text(compact=True) if entry.senses else '?'
+      sense = entry.senses[0].text(compact=True).split('/')[0][0:29] if entry.senses else '?'
       print(word + '\t' + romaji + '\t' + sense)
     else:
       print(word + '\t-\t-')
